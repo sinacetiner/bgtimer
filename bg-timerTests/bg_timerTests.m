785276683ff48b3b7b6488e02d51acc8ca04e981
sc <mahmood1@scMac.local>
2011-12-16 22:17:53 +0100

    Initial Commit

---
+++ bg-timerTests/bg_timerTests.m
@@ -0,0 +1,32 @@
+//
+//  bg_timerTests.m
+//  bg-timerTests
+//
+//  Created by Mahmood1 on 12/16/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "bg_timerTests.h"
+
+@implementation bg_timerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in bg-timerTests");
+}
+
+@end
